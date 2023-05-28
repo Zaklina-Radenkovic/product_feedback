ca97ce6cfb11eb188cb4ac56811bb43bd749db21
Zaklina Radenkovic <zradenkovic@hotmail.com>
2023-05-28 15:36:15 +0200

    adding CategoriesMenu component

---
+++ src/components/CategoriesMenu.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./button/Button";
+
+const CategoriesMenu = () => {
+  return (
+    <div className="bg-white p-5 my-4 rounded-lg flex flex-wrap gap-x-2 gap-y-2.5">
+      <Button className="button-category">All</Button>
+      <Button className="button-category">UI</Button>
+      <Button className="button-category">UX</Button>
+      <Button className="button-category">Enhancement</Button>
+      <Button className="button-category">Feature</Button>
+      <Button className="button-category">Bug</Button>
+    </div>
+  );
+};
+
+export default CategoriesMenu;
