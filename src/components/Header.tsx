97c6f219dcc468fcc4619a85cb211511935b3d74
Zaklina Radenkovic <zradenkovic@hotmail.com>
2023-05-28 15:37:08 +0200

    adding Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-header bg-cover bg-no-repeat pl-5 pt-12 pb-5  rounded-lg">
+      <h1 className="text-white font-bold tracking-tight text-xl">
+        Frontend Mentor
+      </h1>
+      <p className="text-gray-light text-base font-semibold">Feedback board</p>
+    </div>
+  );
+};
+
+export default Header;
