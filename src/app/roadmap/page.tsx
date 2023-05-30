047bbf3ac85a94ebeab738aa557eee6cc5be90cf
Zaklina Radenkovic <zradenkovic@hotmail.com>
2023-05-30 17:19:55 +0200

    cofiguring files

--- src/app/roadmap/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Roadmap() {
-  return (
-    <div className="bg-white p-5 my-4 rounded-lg flex flex-col text-green-600">
-      Roadmap page
-    </div>
-  );
-}
