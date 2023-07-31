8470b78b0dd41c064d67a9c1503e0a23c96dde33
Zaklina Radenkovic <zradenkovic@hotmail.com>
2023-07-31 13:37:46 +0200

    adding style for components

--- src/app/feedback/[feedbackId]/edit/page.tsx
+++ src/app/feedback/[feedbackId]/edit/page.tsx
@@ -1,5 +1,92 @@
+"use client";
+import Image from "next/image";
+import editFeedbackIcon from "../../../../../public/icon-edit-feedback.svg";
+import Button from "@/components/button/Button";
+import ButtonFeedback from "@/components/button/ButtonFeedback";
+import GoBackButton from "@/components/button/GoBackButton";
+import { useFeedbackContext } from "@/state/feedback";
+import { useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
+
 const page = () => {
-  return <div>Edit Suggestion</div>;
+  const { feedbacks } = useFeedbackContext();
+
+  const params = useParams();
+  const { feedbackId } = params;
+
+  const currentFeedback = feedbacks?.find(
+    (feedback) => feedback.id === feedbackId
+  );
+  const router = useRouter();
+
+  return (
+    <div className="w-[490px] mx-auto">
+      <GoBackButton
+        className=" text-gray"
+        stroke="blue"
+        onClick={() => router.back()}
+      />
+      <div className="bg-white text-gray mt-16 pt-14 pl-10 pr-9 pb-9 relative">
+        <Image src={editFeedbackIcon} alt="edit icon" className="edit-icon" />
+        <h2 className="pb-[80px]">Editing '{currentFeedback?.title}'</h2>
+        <form action="" className="flex flex-col">
+          <h5 className="pb-[10px]">Feedback title</h5>
+          <label htmlFor="" className="pb-[20px]">
+            Add a short, descriptive headline
+          </label>
+          <input
+            type="text"
+            placeholder={currentFeedback?.title}
+            className="py-3 pl-5 bg-gray-light rounded-lg mb-6"
+          />
+
+          <h5 className="pb-[10px]">Category</h5>
+          <label htmlFor="" className="pb-[20px]">
+            Choose a category for your feedback
+          </label>
+          <input
+            type="text"
+            placeholder={currentFeedback?.category}
+            className="py-3 pl-5 bg-gray-light rounded-lg mb-6"
+          />
+
+          <h5 className="pb-[10px]">Update status</h5>
+          <label htmlFor="" className="pb-[20px]">
+            Change feature state
+          </label>
+          <input
+            type="text"
+            placeholder={currentFeedback?.status}
+            className="py-3 pl-5 bg-gray-light rounded-lg mb-6"
+          />
+
+          <h5 className="pb-[10px]">Feedback detail</h5>
+          <label htmlFor="" className="pb-[20px]">
+            Include any specific comments on what should be improved, added,
+            etc.
+          </label>
+          <input
+            type="text"
+            placeholder={currentFeedback?.description}
+            className="py-3 pl-5 bg-gray-light rounded-lg mb-6"
+          />
+        </form>
+        <div className="flex items-center justify-between">
+          <Button className="py-3 px-5 bg-red text-white rounded-lg">
+            Delete
+          </Button>
+          <div className="">
+            <Button className="py-3 px-5 bg-gray mr-3 text-white rounded-lg">
+              Cancel
+            </Button>
+            <ButtonFeedback className="button-edit" onClick={() => {}}>
+              Save Changes
+            </ButtonFeedback>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/app/feedback/[feedbackId]/page.tsx
+++ src/app/feedback/[feedbackId]/page.tsx
@@ -1,38 +1,48 @@
 "use client";
+import { useRouter } from "next/navigation";
 import ButtonFeedback from "@/components/button/ButtonFeedback";
 import GoBackButton from "@/components/button/GoBackButton";
-import SuggestionItem from "@/components/feedbacks/FeedbackCard";
+import FeedbackCard from "@/components/feedbacks/FeedbackCard";
 import { useFeedbackContext } from "@/state/feedback";
-import { SortedFeedbackProvider } from "@/state/sortedFeedback";
+
 import { useParams } from "next/navigation";
+import Comments from "@/components/comments/Comments";
+import Form from "@/components/form/Form";
 
 const page = () => {
+  const router = useRouter();
   const { feedbacks } = useFeedbackContext();
+
   const params = useParams();
-  const { suggestionId } = params;
-  console.log(suggestionId);
-  return (
-    <>
-      <header className="flex flex-row justify-between items-center">
-        <GoBackButton className=" text-gray " stroke="blue" />
-        {/* {suggestionId} */}
-        <ButtonFeedback className="button-edit">Edit Feedback</ButtonFeedback>
-      </header>
-      <SuggestionItem
-        sortedFeedback={{
-          id: suggestionId,
-          title: "",
-          category: "",
-          upvotes: 0,
-          status: "",
-          description: "",
-          comments: [],
-        }}
-      />
-      <div>Comments</div>
-      <div>Add Comment</div>
-    </>
+  const { feedbackId } = params;
+
+  const currentFeedback = feedbacks?.find(
+    (feedback) => feedback.id === feedbackId
   );
+  console.log(currentFeedback);
+  if (currentFeedback)
+    return (
+      <div className="w-[51.5rem] mx-auto">
+        <header className="flex flex-row justify-between items-center">
+          <GoBackButton
+            className=" text-gray"
+            stroke="blue"
+            onClick={() => router.back()}
+          />
+          <ButtonFeedback
+            className="button-edit"
+            onClick={() => router.push(`${feedbackId}/edit`)}
+          >
+            Edit Feedback
+          </ButtonFeedback>
+        </header>
+        <FeedbackCard feedback={currentFeedback} />
+        {currentFeedback.comments?.length >= 1 && (
+          <Comments feedback={currentFeedback} />
+        )}
+        <Form />
+      </div>
+    );
 };
 
 export default page;
