633fb4aefee7240ae6b2f0a81a50bc5e80c71f3e
Zaklina Radenkovic <zradenkovic@hotmail.com>
2023-09-21 10:47:36 +0200

    dynamically set styling

--- src/components/CategoriesMenu.tsx
+++ src/components/CategoriesMenu.tsx
@@ -3,14 +3,18 @@ import { useFeedbackContext } from "@/state/feedback";
 import Button from "./button/Button";
 
 const CategoriesMenu = () => {
-  const { setCategory } = useFeedbackContext();
+  const { setCategory, loading } = useFeedbackContext();
 
   const filterTasks = (event: React.BaseSyntheticEvent) => {
     setCategory(event.currentTarget.dataset["filter"]);
   };
 
   return (
-    <div className="box p-6 my-[25px] flex flex-wrap gap-x-2 gap-y-3">
+    <div
+      className={`box p-6 my-[25px] flex flex-wrap gap-x-2 gap-y-3 ${
+        !loading ? "blur-[2px]" : "blur-none"
+      }`}
+    >
       <Button
         className="button-category"
         data-filter="All"
