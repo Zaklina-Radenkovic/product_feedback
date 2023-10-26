1cf96136e40ff67f2b40bccf1ef3dd8c55ed9825
Zaklina Radenkovic <zradenkovic@hotmail.com>
2023-10-26 17:53:07 +0200

    adding link to button

--- src/app/roadmap/components/RoadmapHeader.tsx
+++ src/app/roadmap/components/RoadmapHeader.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 import ButtonFeedback from "../../../components/button/ButtonFeedback";
 import GoBackButton from "../../../components/button/GoBackButton";
 
 const RoadmapHeader = () => {
+  const router = useRouter();
+
   return (
     <header className="navbar p-[1.875rem] font-bold mb-0">
       <div className="grid grid-rows-2 gap-2.5">
@@ -11,7 +16,11 @@ const RoadmapHeader = () => {
         </Link>
         <h3>Roadmap</h3>
       </div>
-      <ButtonFeedback> &#43; Add feedback </ButtonFeedback>
+
+      <ButtonFeedback onClick={() => router.push("/create-feedback")}>
+        {" "}
+        &#43; Add feedback{" "}
+      </ButtonFeedback>
     </header>
   );
 };
