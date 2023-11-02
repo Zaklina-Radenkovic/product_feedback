2ec8a4cb066ff1a98092396d8fe5a30289e56e13
Zaklina Radenkovic <zradenkovic@hotmail.com>
2023-11-02 12:06:23 +0100

    updating classes

--- src/components/button/ButtonVote.tsx
+++ src/components/button/ButtonVote.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-import Button from "./Button";
-import arrowIconUp from "../../../public/icon-arrow-up.svg";
+import Button from './Button';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -9,13 +7,16 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   upvoted: boolean | undefined;
 }
 
-const ButtonVote = ({ children, onClick, className, upvoted }: Props) => {
+const ButtonVote = ({ children, onClick, upvoted, className }: Props) => {
   return (
-    <Button className={className} onClick={onClick}>
+    <Button
+      className={`${className} + md:btn-upvote-md btn-upvote-x items-center gap-2.5 rounded-lg bg-gray-light p-2.5 text-[13px] font-bold text-secondary`}
+      onClick={onClick}
+    >
       <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M1 6l4-4 4 4"
-          stroke={upvoted ? "#fff" : "#4661E6"}
+          stroke={upvoted ? '#fff' : '#4661E6'}
           strokeWidth="2"
           fill="none"
           fillRule="evenodd"
